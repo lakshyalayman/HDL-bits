dba22c7cbba3a083cde7abf8110e7be3b52f7085
lakshyalayman <lakshya1.web@gmail.com>
2025-08-27 16:48:58 +0530

    vamos

--- Procedures/always_nolatches.v
+++ Procedures/always_nolatches.v
@@ -3,6 +3,15 @@ module top_module (
     output reg left,
     output reg down,
     output reg right,
-    output reg up  ); 
-
+    output reg up  );
+  always @(*) begin 
+    left = 1'b0;down = 1'b0;up = 1'b0;right = 1'b0;
+    case (scancode)
+      16'he06b: left = 1;
+      16'he072: down = 1;
+      16'he074: right = 1;
+      16'he075: up = 1;
+      default: begin left = 1'b0;down = 1'b0;up = 1'b0;right = 1'b0;end
+    endcase
+  end
 endmodule

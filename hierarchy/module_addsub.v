dc77e50b034e5fa8307191029d9c6d22e33ccd72
lakshyalayman <lakshya1.web@gmail.com>
2025-08-27 13:55:15 +0530

    hola

--- hierarchy/module_addsub.v
+++ hierarchy/module_addsub.v
@@ -4,5 +4,7 @@ module top_module(
     input sub,
     output [31:0] sum
 );
-  wire cout;
+  wire cout,cd;
+  add16 inst1 (a[15:0],{16{sub}}^b[15:0],sub,sum[15:0],cout);
+  add16 inst2 (a[31:16],{16{sub}}^b[31:16],cout,sum[31:16],cd);
 endmodule
